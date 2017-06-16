c2a4dc9583639cc51c3cc4285421fb91078f9243
Pandas <daria-shcherbatyuk@yandex.ru>
2017-06-16 23:16:54 +0300

    update singleton

--- singleton.hpp
+++ singleton.hpp
@@ -1,18 +1,22 @@
 #ifndef SINGLETON_HPP
 #define SINGLETON_HPP
-
-template<typename T>
-class singleton {
+template <typename T>
+class Singleton
+{
 public:
-    static T& instance() {
-        if(!myInstance)
-            myInstance = new T;
-        return *myInstance;
-    }
-
+  static T& instance()
+  {
+    if(myInstance == nullptr)
+      myInstance = new T;
+      return *myInstance;
+  }
 private:
-    static T* myInstance;
-    singleton (const singleton&){}
+  static T* myInstance;
+  Singleton() = delete;
+  Singleton(const Singleton<T>&) = delete;
 };
+
+template <typename T>
+T* Singleton::myInstance = nullptr;
 
 #endif // SINGLETON_HPP
