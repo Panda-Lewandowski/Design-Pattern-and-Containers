2f954391bebe18721490c780305b577062224ae4
Pandas <daria-shcherbatyuk@yandex.ru>
2017-06-17 17:45:15 +0300

    update class adapter

--- class_adapter.hpp
+++ class_adapter.hpp
@@ -1,4 +1,52 @@
 #ifndef ADAPTER_HPP
 #define ADAPTER_HPP
 
+class Shape {
+public:
+    Shape();
+    virtual void BoundingBox(Point& bottomLeft, Point& topRight ) const;
+    virtual Manipulator* CreateManipulator() const;
+};
+
+
+class TextView {
+public:
+    TextView();
+    void GetOrigin(Coord& x, Coord& y) const;
+    void GetExtent(Coord& width, Coord& height) const;
+    virtual bool IsEmpty() const;
+};
+
+
+class TextShape : public Shape, private TextView {
+public:
+    TextShape();
+    virtual void BoundingBox(Point& bottomLeft, Point& topRight) const;
+    virtual bool IsEmpty() const;
+    virtual Manipulator* CreateManipulator() const;
+};
+
+void TextShape::BoundingBox (Point& bottomLeft, Point& topRight) const
+{
+    Coord bottom, left, width, height;
+
+    GetOrigin(bottom, left);
+    GetExtent(width, height);
+
+    bottomLeft = Point(bottom, left);
+    topRight = Point(bottom + height, left + width);
+    //..
+}
+
+
+bool TextShape::IsEmpty () const
+{
+    return TextView::IsEmpty();
+}
+
+Manipulator* TextShape::CreateManipulator () const
+{
+    return new TextManipulator(this);
+}
+
 #endif // ADAPTER_HPP
