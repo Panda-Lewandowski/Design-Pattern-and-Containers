e68022bd15d677281322a59233def2e49b802091
Pandas <daria-shcherbatyuk@yandex.ru>
2017-06-19 00:01:47 +0300

    add command

---
+++ command.hpp
@@ -0,0 +1,24 @@
+#ifndef COMMAND_HPP
+#define COMMAND_HPP
+class Command //базовый класс команды
+{
+public:
+    virtual bool execute() = 0;
+};
+
+template<typename Reciever>
+class SimpleCommand: public Command
+{
+private:
+    typedef bool (Reciever::*Action)();
+    Reciever* rec; //указатель на объект
+    Action act; //указатель на метод
+
+public:
+    SimpleCommand(Reciever* r, Action a); //конструктор
+    bool execute() //экзекутор
+    {
+        return (rec->*act)();
+    }
+};
+#endif // COMMAND_HPP
