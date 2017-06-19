94b8cf18984a4c2f544fb08fc9415bcfc3b55f5d
Pandas <daria-shcherbatyuk@yandex.ru>
2017-06-19 23:18:12 +0300

    add observer

---
+++ observer.hpp
@@ -0,0 +1,27 @@
+#ifndef OBSERVER_HPP
+#define OBSERVER_HPP
+delegate void EventHandler(Object^ source, double a);
+
+public ref class Manager //издатель
+{
+public:
+    event EventHandler^ OnHandler;
+    void method()
+    {
+        OnHandler(this, 10.);
+    }
+}
+public ref class Watcher //подписчик
+{
+public:
+    Watcher(Manager^ m)
+    {
+        m->OnHandler += gcnew EventHandler(this, &Watcher::f) //& - потомучто метод класса
+    }
+    void f(Object^ source, double a) //подписали метод f на возникновение события. Когда начнёт выполняться method, будут выполняться методы классов, подписанных на возникновение события
+    {
+    //...
+    }
+}
+}
+#endif // OBSERVER_HPP
